0c06d5a83b84f9a4d7b26005d5c2a936ba6d1609
jinsujj <wlstncjs1234@naver.com>
2024-07-09 23:52:21 +0900

    변경사항: 공중 유닛은 지형에 상관없이 움직일 수 있지만, 그라운드 유닛은 지형에 영향을 받으면서 움직일 수 있다.
    또한 공격도, 지상공격만 할 수 있는 유닛과, 공중공격만 할 수 있는 유닛,
    그리고 둘다 가능한 유닛이 있다

---
+++ src/main/java/com/example/starcraft/AttackType.java
@@ -0,0 +1,31 @@
+package com.example.starcraft;
+
+/*
+ *  책임: 공격 가능 타입을 정의
+ *  메시지: canAttack();
+ */
+
+public class AttackType {
+    private boolean canAttackGround;
+    private boolean canAttackAir;
+    private boolean canAttackBoth;
+
+    public AttackType(boolean canAttackGround, boolean canAttackAir, boolean canAttackBoth){
+        this.canAttackGround = canAttackGround;
+        this.canAttackAir =canAttackAir;
+        this.canAttackBoth = canAttackBoth;
+    }
+
+    public boolean canAttack(Unit target) {
+        // TODO 현재 공중 유닛은 지상, 공중 모두 가능 변경 필요
+
+        if (target instanceof GroundUnit){
+            return canAttackGround;
+        }
+        else if (target instanceof AirUnit){
+            return canAttackAir || canAttackBoth;
+        }
+        return false;
+    }
+
+}
